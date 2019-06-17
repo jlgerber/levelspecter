23669f91dec43fabb3ce1a2b02846475dacae584
Jonathan Gerber <jgerber@jg-mbp-2019.local>
2019-06-17 08:12:05 -0700

    adds assetdev sequence tp parsing

--- src/levelparser.rs
+++ src/levelparser.rs
@@ -3,11 +3,13 @@ use nom::{
     branch::alt,
     combinator::{all_consuming},
     bytes::complete::{tag},
+    character::complete::digit1,
     sequence::{tuple, preceded },
     multi::{ fold_many1},
 };
+
 use crate::LevelSpecterError;
-use aschar_casesensitive::{ upperalphanum1, alpha_alphanum_upper};
+use aschar_casesensitive::{ /*upperalphanum1,*/ alpha_alphanum_upper};
 
 #[inline]
 fn parse_show(input: &str) -> IResult<&str, &str> {
@@ -30,7 +32,26 @@ fn parse_seq(input: &str) -> IResult<&str, &str> {
 #[inline]
 fn parse_shot(input: &str) -> IResult<&str, &str> {
     alt((
-    preceded(tag("."), upperalphanum1 ),
+    preceded(tag("."), digit1 ),
+    preceded(tag("."), tag("%"))
+    ))
+    (input)
+}
+
+// NOTE: if I decide to go case insensitive, there is tag_no_case()
+#[inline]
+fn parse_assetdev_seq(input: &str) -> IResult<&str, &str> {
+    alt((
+        preceded(tag("."), tag("ASSETDEV")),
+        preceded(tag("."), tag("%"))
+    ))
+    (input)
+}
+
+#[inline]
+fn parse_assetdev_shot(input: &str) -> IResult<&str, &str> {
+    alt((
+    preceded(tag("."), alpha_alphanum_upper ),
     preceded(tag("."), tag("%"))
     ))
     (input)
@@ -41,7 +62,10 @@ fn parse_shot(input: &str) -> IResult<&str, &str> {
 #[inline]
 fn shot_alt(input: &str) -> IResult<&str, Vec<&str>> {
     fold_many1( //used to turn the tuple into a vector
-        tuple(( parse_show, parse_seq, parse_shot)),
+        alt((
+            tuple(( parse_show, parse_seq, parse_shot)),
+            tuple((parse_show, parse_assetdev_seq, parse_assetdev_shot))
+        )),
         Vec::new(), 
         |mut acc: Vec<_>, item| {
             let (show,seq,shot) = item;
@@ -105,7 +129,144 @@ pub fn levelspec_parser(input: &str) -> Result<Vec<&str>, LevelSpecterError> {
 }
 
 #[cfg(test)]
-mod tests {
+mod levelspec {
+    use super::*;
+
+    mod show {
+        use super::*;
+            
+        #[test]
+        fn can_parse() {
+            let ls = levelspec_parser("DEV01");
+            assert_eq!(ls, Ok(vec!["DEV01"]))
+        }
+
+        #[test]
+        fn cannot_start_with_number() {
+            let ls = levelspec_parser("1DEV01");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for 1DEV01".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_space() {
+            let ls = levelspec_parser("DEV 01");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV 01".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_wildcard_and_chars() {
+            let ls = levelspec_parser("DEV%01");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV%01".to_string())));
+        }
+
+        #[test]
+        fn cannot_have_underscore() {
+            let ls = levelspec_parser("DEV_01");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV_01".to_string())));
+        }
+
+        #[test]
+        fn can_parse_show_wildcard() {
+            let ls = levelspec_parser("%");
+            assert_eq!(ls, Ok(vec!["%"]))
+        }
+    }
+
+    mod seq {
+        use super::*;
+            
+        #[test]
+        fn can_parse() {
+            let ls = levelspec_parser("DEV01.RD");
+            assert_eq!(ls, Ok(vec!["DEV01", "RD"]))
+        }
+    
+        #[test]
+        fn can_parse_assetdev() {
+            let ls = levelspec_parser("DEV01.ASSETDEV");
+            assert_eq!(ls, Ok(vec!["DEV01", "ASSETDEV"]))
+        }
+
+        #[test]
+        fn cannot_start_with_number() {
+            let ls = levelspec_parser("DEV01.1D");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.1D".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_space() {
+            let ls = levelspec_parser("DEV01.R D");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.R D".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_wildcard_and_chars() {
+            let ls = levelspec_parser("DEV01.R%");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.R%".to_string())));
+        }
+
+        #[test]
+        fn cannot_have_underscore() {
+            let ls = levelspec_parser("DEV01.R_D");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.R_D".to_string())));
+        }
+
+        #[test]
+        fn can_parse_wildcard() {
+            let ls = levelspec_parser("DEV01.%");
+            assert_eq!(ls, Ok(vec!["DEV01","%"]))
+        }
+    }
+
+    mod shot {
+        use super::*;
+    
+        #[test]
+        fn can_parse() {
+            let ls = levelspec_parser("DEV01.RS.0001");
+            assert_eq!(ls, Ok(vec!["DEV01", "RS", "0001"]))
+        }
+
+        #[test]
+        fn can_parse_assetdev() {
+            let ls = levelspec_parser("DEV01.ASSETDEV.FOOBAR");
+            assert_eq!(ls, Ok(vec!["DEV01", "ASSETDEV", "FOOBAR"]))
+        }
+
+        #[test]
+        fn cannot_start_with_letter() {
+            let ls = levelspec_parser("DEV01.RD.R0001");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.RD.R0001".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_space() {
+            let ls = levelspec_parser("DEV01.RD.0 001");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.RD.0 001".to_string())));
+        }
+        
+        #[test]
+        fn cannot_have_wildcard_and_chars() {
+            let ls = levelspec_parser("DEV01.RD.00%");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.RD.00%".to_string())));
+        }
+
+        #[test]
+        fn cannot_have_underscore() {
+            let ls = levelspec_parser("DEV01.RD.0_001");
+            assert_eq!(ls, Err(LevelSpecterError::ParseError("Unable to parse levelspec for DEV01.RD.0_001".to_string())));
+        }
+
+        #[test]
+        fn can_parse_wildcard() {
+            let ls = levelspec_parser("DEV01.RS.%");
+            assert_eq!(ls, Ok(vec!["DEV01", "RS", "%"]))
+        }
+    }
+}
+
+#[cfg(test)]
+mod atoms {
     use super::*;
 
     #[test]
@@ -114,7 +275,7 @@ mod tests {
         assert!(result.is_ok());
         assert_eq!(result, Ok(("", "%")));
     }
-    
+
     #[test]
     fn seq_can_parse_wildcard() {
         let result = parse_seq(".%");
@@ -128,4 +289,5 @@ mod tests {
         assert!(result.is_ok());
         assert_eq!(result, Ok(("", "%")));
     }
+    
 }
