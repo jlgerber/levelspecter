47df6ae9f9f70612eecf26f007a0bfc114918175
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-16 23:09:02 -0700

    makes parsing simpler

--- src/levelparser.rs
+++ src/levelparser.rs
@@ -4,47 +4,47 @@ use nom::{
     combinator::{all_consuming, recognize},
     bytes::complete::{tag},
     sequence::{tuple, preceded },
-    multi::many_m_n,
+    multi::{many_m_n, many0},
 };
 
-use aschar_casesensitive::{ upperalphanum1, alpha_alphanum};
+use aschar_casesensitive::{ upperalphanum1, alpha_alphanum, alpha_alphanum_upper};
 
 
 // fn alpha_alphanum(input: &str) -> IResult<&str, &str> {
 //     recognize(tuple((upperalpha1, upperalphanum0)))(input)
 // }
 
+#[inline]
+fn recognize_seq(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((tag("."), alpha_alphanum_upper)))(input)
+}
+
+
+#[inline]
+fn recognize_shot(input: &str) -> IResult<&str, &str> {
+    recognize(tuple((tag("."), upperalphanum1 )))(input)
+}
+
 fn levelparser(input: &str) -> IResult<&str, String> {
     let result = all_consuming(
         tuple(( // inner tuple start
             alpha_alphanum, // show has to start with a letter
-            many_m_n(
-                0, 
-                1, 
+            many0( 
                 alt((
                     recognize( 
                         tuple((
-                            preceded(
-                                tag("."),
-                                alpha_alphanum
-                            ), 
-                            preceded(
-                                tag("."), 
-                                upperalphanum1
-                            )
+                            recognize_seq, 
+                            recognize_shot
                         ))
                     ),
-                    preceded(
-                        tag("."),
-                        alpha_alphanum
-                    )
-                 ))
-            ) // many_m_n end  
+                    recognize_seq
+                ))
+            ) // many0 end  
         )) // tuple end
     )(input)?;
     
     let (_,(show, mut seqshot)) = result; 
-
+    println!("seqshot {:?}", seqshot);
     let seqshot = match seqshot.len() {
         2 => seqshot.join("."),
         1 =>  {
