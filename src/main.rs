65cf2c78cecdad5616e1706cf819e00e062e9687
Jonathan Gerber <jgerber@jg-mbp-2019.home>
2019-06-18 08:00:24 -0700

    main parses levelspec from commandline

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
-use levelspecter::levelspec_parser;
+use levelspecter::{LevelSpec, LevelSpecterError};
+use std::env;
 
-
-fn main() {
-    println!("levelspec_parser(\"DEV01.RD.9999.343\") {:?}",levelspec_parser("DEV01.RD.9999.343"));
-    println!("levelspec_parser(\"DEV01.rd.9999\") {:?}",levelspec_parser("DEV01.rd.9999"));
-
-    println!("levelspec_parser(\"DEV01.RD.9999\") {:?}",levelspec_parser("DEV01.RD.9999"));
-    println!("levelspec_parser(\"DEV01.RD\") {:?}",levelspec_parser("DEV01.RD"));
-    println!("levelspec_parser(\"DEV01\") {:?}",levelspec_parser("DEV01"));
-
+fn main() -> Result<(), LevelSpecterError> {
+    let args = env::args();
+    if args.len() < 2 {
+        eprintln!("levelspecter <levelspec>");
+        std::process::exit(1);
+    }
+    let args = args.collect::<Vec<_>>();
+    let levelspec = LevelSpec::new(&args[1])?;
+    println!("{:?}", levelspec);
+    Ok(())
 }
